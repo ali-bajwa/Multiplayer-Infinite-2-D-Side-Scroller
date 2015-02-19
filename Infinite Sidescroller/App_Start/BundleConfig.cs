9f6ce0d8c428babb2bbf091b81427e5fe4dcfe5f
Ali Bajwa <ali.z.bajwa@gmail.com>
2015-02-18 23:42:24 -0600

    Attempt at fixing DB issues being experienced by everyone (custom code to wipe DB and attempt a rewrite based on updated model)

--- Infinite Sidescroller/App_Start/BundleConfig.cs
+++ Infinite Sidescroller/App_Start/BundleConfig.cs
@@ -14,15 +14,6 @@ namespace Infinite_Sidescroller
       bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                   "~/Scripts/jquery.validate*"));
 
-      // Use the development version of Modernizr to develop with and learn from. Then, when you're
-      // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                  "~/Scripts/modernizr-*"));
-
-      bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
       bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/global.css"));
     }
--- Infinite Sidescroller/App_Start/Startup.Auth.cs
+++ Infinite Sidescroller/App_Start/Startup.Auth.cs
@@ -8,6 +8,7 @@ using System;
 using System.Web.Helpers;
 using System.Security.Claims;
 using System.Configuration;
+using Infinite_Sidescroller.Models;
 
 namespace Infinite_Sidescroller
 {
@@ -16,29 +17,24 @@ namespace Infinite_Sidescroller
     // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
     public void ConfigureAuth(IAppBuilder app)
     {
+      app.CreatePerOwinContext(ApplicationDbContext.Create);
+      app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+      app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+
       // Enable the application to use a cookie to store information for the signed in user
       app.UseCookieAuthentication(new CookieAuthenticationOptions
       {
         AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-        LoginPath = new PathString("/Account/Login")
+        LoginPath = new PathString("/Account/Login"),
+        Provider = new CookieAuthenticationProvider
+        {
+          // Enables the application to validate the security stamp when the user logs in.
+          // This is a security feature which is used when you change a password or add an external login to your account.  
+          OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+              validateInterval: TimeSpan.FromMinutes(30),
+              regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+        }
       });
-      // Use a cookie to temporarily store information about a user logging in with a third party login provider
-      app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-      // Uncomment the following lines to enable logging in with third party login providers
-      //app.UseMicrosoftAccountAuthentication(
-      //    clientId: "",
-      //    clientSecret: "");
-
-      //app.UseTwitterAuthentication(
-      //   consumerKey: "",
-      //   consumerSecret: "");
-
-      //app.UseFacebookAuthentication(
-      //   appId: "",
-      //   appSecret: "");
-
-      //app.UseGoogleAuthentication();
     }
   }
 }

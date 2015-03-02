12dc4264a5555ff42fa4fda05c4f315d92711f27
unknown <klayton21@live.missouristate.edu>
2015-03-01 19:18:37 -0600

    Removed jumping to work on box2d

--- Infinite Sidescroller/Content/gamedemo/Controllers/GameController.js
+++ Infinite Sidescroller/Content/gamedemo/Controllers/GameController.js
@@ -11,10 +11,6 @@ var GameController = (function(){
 
 	var MOVEMENT_EDGE = 500; // where terrain start scrolling
 
-	var vertical_velocity = 0;
-	var delta_s = 0;
-	var movement_modifier = 1;
-
 	var update_all = function(event){
 		/*
 		 * main function pretty much
@@ -40,32 +36,10 @@ var GameController = (function(){
 			}
 		}
 
-
-
-		if (cmds.indexOf("up") > -1) {
-		    if (GameModel.hero.y == 510) {
-		        vertical_velocity = 450;
-		        movement_modifier = 0.3;
-		    }
-		}
-
-		vertical_velocity -= event.delta / 1000 * 700;
-		delta_s = vertical_velocity * event.delta / 1000;
-
 		if(cmds.indexOf("left") > -1){
 			if(GameModel.hero.x > 10){
 				PlayerController.move_left(GameModel.hero);
 			}
-		}
-
-		if ((GameModel.hero.y - delta_s) <= 510) {
-		    GameModel.hero.y -= delta_s;
-		}
-		else {
-		    GameModel.hero.y = 510;
-		}
-		if (GameModel.hero.y == 510) {
-		    movement_modifier = 1;
 		}
 
 		// <<<
